d00d9c4abca8b9fa2548dd764d002765e16d61e9
Kyjorn <hendonmshane@gmail.com>
2025-05-04 15:24:08 -0600

    Implement countdown from 10 in From10 class

--- From10.java
+++ From10.java
@@ -2,6 +2,8 @@ public class From10 {
 
     public static void main(String[] args) {
         
-        
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
     }
 }
